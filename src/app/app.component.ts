98b4e69711b59587e5b28230501b4f721283b0fd
faisal ahmed <mishu_dc@yahoo.com>
2019-09-22 22:40:39 -0400

    ngx-scrollspy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ScrollSpyService } from '@uniprank/ngx-scrollspy';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'scroll-spy';
-  selected = "section2";
+  selected = 'section2';
 
-  changeSelected(value:string){
-    if(value!=this.selected){
+  constructor(private _scrollSpyService: ScrollSpyService) {}
+
+  ngOnInit() {
+      this._scrollSpyService.setOffset('window', 100);
+  }
+
+  changeSelected(value: string) {
+    if (value !== this.selected) {
       this.selected = value;
     }
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxScrollspyModule } from '@uniprank/ngx-scrollspy';
+
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
 
@@ -10,7 +12,8 @@ import { ContentComponent } from './content/content.component';
     ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxScrollspyModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
